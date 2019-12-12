ed7461de4bba06fe2d2e329dafad96d1b404a4b6
Laxybaxy <50335046+Laxybaxy@users.noreply.github.com>
2019-12-12 20:07:32 +0100

    Add files via upload
    added mkdir in exepath, dbg mode

--- Backuper/Backuper.cpp
+++ Backuper/Backuper.cpp
@@ -3,8 +3,16 @@
 #include <fstream>
 #include <iomanip>
 #include <string>
+#include <direct.h>
 #include "json.hpp"
 using namespace std;
+
+std::string ExePath() {
+    char buffer[MAX_PATH];
+    GetModuleFileName(NULL, buffer, MAX_PATH);
+    string::size_type pos = string(buffer).find_last_of("\\/");
+    return string(buffer).substr(0, pos);
+}
 
 int main(int argc, char* argv[])
 {
@@ -18,8 +26,8 @@ int main(int argc, char* argv[])
     else { //if no cfg makes default
         printf("creating config.json\n");
 
-        j["path"] = { "C:\\test\\ye.txt" };
-        j["destination"] = "C:\\test\\files\\";
+        j["dbg"] = true;
+        j["src"] = { "C:/test/ye.txt","C:/test/ya.txt" };
 
         //  Write to cfg
         std::ofstream o("config.json");
@@ -28,21 +36,28 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    for (int i = 0; i < j["path"].size(); i++)
-    {
-        string tmpdest = j["destination"];
-        string src = j["path"].at(i);
-        std::size_t found = src.find_last_of("/\\");
-        string dest = tmpdest + src.substr(found+1);
+    string dir = ExePath()+"/files";
+    mkdir(dir.c_str());
+    
 
-        printf("Src: %s - Dest: %s - ", src.c_str(),dest.c_str());
-        if (CopyFile(src.c_str(), dest.c_str(), false)) {
-            printf("Done!\n");
-        }
-        else
-        {
-            printf("Failed!\n");
+    for (int i = 0; i < j["src"].size(); i++)
+    {
+        string src = j["src"].at(i);
+        std::size_t found = src.find_last_of("/\\");
+        string dest = ExePath() + "\\files\\" + src.substr(found+1);
+
+        if (j["dbg"] == true) {
+            printf("Src: %s - Dest: %s - ", src.c_str(), dest.c_str());
+            if (CopyFile(src.c_str(), dest.c_str(), false)) {
+                printf("Done!\n");
+            }
+            else
+            {
+                printf("Failed!\n");
+            }
         }
     }
+    if(j["dbg"]==true)
+        system("pause");
     return 1;
 }
